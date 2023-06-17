d12573e4823dc2a68279676b671b107ce93dabf6
Ivanna-Daniela <ivanna.cevallos@epn.edu.ec>
2023-06-17 05:15:57 -0500

    Operation Pharmacy
    Create pharmacy, update pharmacy, read pharmacy

--- 00-Kotlin/untitled/src/main/kotlin/Main.kt
+++ 00-Kotlin/untitled/src/main/kotlin/Main.kt
@@ -1,193 +1,49 @@
-import java.util.*
+import java.io.BufferedWriter
+import java.io.File
+import java.io.FileWriter
+import java.io.IOException
 
-fun main(args: Array<String>) {
-    println("Hello World!")
-    // Tipos de variables
-    // INMUTABLES (NO se reasignan "=")
-    val inmutable: String = "Adrian";
-    // inmutable = "Vicente";
-
-    // Mutables (Re asignar)
-    var mutable: String = "Vicente";
-    mutable = "Adrian";
-
-
-    //  val > var
-
-    // Duck Typing
-    var ejemploVariable = " Adrian Eguez "
-    val edadEjemplo: Int = 12
-    ejemploVariable.trim()
-    // ejemploVariable = edadEjemplo;
-
-    // Variable primitiva
-    val nombreProfesor: String = "Adrian Eguez"
-    val sueldo: Double = 1.2
-    val estadoCivil: Char = 'C'
-    val mayorEdad: Boolean = true
-    // Clases Java
-    val fechaNacimiento: Date = Date()
-
-    // SWITCH
-    val estadoCivilWhen = "C"
-    when (estadoCivilWhen){
-        ("C") -> {
-            println("Casado")
-        }
-        "S" -> {
-            println("Soltero")
-        }
-        else -> {
-            println("No sabemos")
-        }
-    }
-    val coqueteo = if (estadoCivilWhen == "S") "Si" else "No"
-
-    calcularSueldo(10.00)
-    calcularSueldo(10.00, 12.00, 20.00)
-    calcularSueldo(10.00, bonoEspecial = 20.00) // Named Parameters
-    calcularSueldo(bonoEspecial = 20.00, sueldo = 10.00, tasa = 14.00) //  Parametros nombrados
-
-    val sumaUno = Suma(1,1)
-    val sumaDos = Suma(null, 1)
-    val sumaTres = Suma(1, null)
-    val sumaCuatro = Suma(null, null)
-    sumaUno.sumar()
-    sumaDos.sumar()
-    sumaTres.sumar()
-    sumaCuatro.sumar()
-    println(Suma.pi)
-    println(Suma.elevarAlCuadrado(2))
-    println(Suma.historialSumas)
-
-    // ARREGLOS
-
-    // Tipos de Arreglos
-
-    // Arreglo Estatico
-    val arregloEstatico: Array<Int> = arrayOf<Int>(1, 2, 3)
-    println(arregloEstatico)
-
-    // Arreglo Dinámicos
-    val arregloDinamico: ArrayList<Int> = arrayListOf<Int>(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
-    println(arregloDinamico)
-    arregloDinamico.add(11)
-    arregloDinamico.add(12)
-    println(arregloDinamico)
-
-
-
-
-}
-
-// void -> Unit
-fun imprimirNombre(nombre: String): Unit{
-    println("Nombre : ${nombre}")
-}
-
-fun calcularSueldo(
-    sueldo: Double, // Requerido
-    tasa: Double = 12.00, // Opcional (defecto)
-    bonoEspecial: Double? = null, // Opcion null -> nullable
-): Double{
-    // Int -> Int? (nullable)
-    // String -> String? (nullable)
-    // Date -> Date? (nullable)
-    if(bonoEspecial == null){
-        return sueldo * (100/tasa)
-    }else{
-        return sueldo * (100/tasa ) + bonoEspecial
-    }
-}
-
-abstract class NumerosJava{
-    protected val numeroUno: Int
-    private val numeroDos: Int
-    constructor(
-        uno: Int,
-        dos: Int
-    ){ // Bloque de codigo del constructor
-        this.numeroUno = uno
-        this.numeroDos = dos
-        println("Inicializando")
-    }
-}
-
-abstract class Numeros( // Constructor PRIMARIO
-    // Ejemplo:
-    // uno: Int, (Parametro (sin modificador de acceso))
-    // private var uno: Int, // Propiedad Publica Clase numeros.uno
-    // var uno: Int, // Propiedad de la clase (por defecto es PUBLIC)
-    // public var uno: Int,
-    protected val numeroUno: Int, // Propiedad de la clase protected numeros.numeroUno
-    protected val numeroDos: Int, // Propiedad de la clase protected numeros.numeroDos
-){
-    // var cedula: string = "" (public es por defecto)
-    // private valorCalculado: Int = 0 (private)
-    init {
-        this.numeroUno; this.numeroDos; // this es opcional
-        numeroUno; numeroDos; // sin el "this", es lo mismo
-        println("Inicializando")
-    }
-}
-
-
-class Suma( // Constructor Primario Suma
-    uno: Int, // Parametro
-    dos: Int // Parametro
-) : Numeros(uno, dos) { // <- Constructor del Padre
-    init { // Bloque constructor primario
-        this.numeroUno; numeroUno;
-        this.numeroDos; numeroDos;
+object WriteFile {
+    /**
+     * This class shows how to write file in Kotlin
+     * @param args
+     * @throws IOException
+     */
+    @JvmStatic
+    fun main(args: Array<String>) {
+        val data = "I am writing this String to File in Kotlin"
+        writeUsingBufferedWriter(data, 3)
+        println("Done")
     }
 
-    constructor(//  Segundo constructor
-        uno: Int?, // parametros
-        dos: Int // parametros
-    ) : this(  // llamada constructor primario
-        if (uno == null) 0 else uno,
-        dos
-    ) { // si necesitamos bloque de codigo lo usamos
-        numeroUno;
-    }
-
-    constructor(//  tercer constructor
-        uno: Int, // parametros
-        dos: Int? // parametros
-    ) : this(  // llamada constructor primario
-        uno,
-        if (dos == null) 0 else uno
-    ) // Si no lo necesitamos al bloque de codigo "{}" lo omitimos
-
-    constructor(//  cuarto constructor
-        uno: Int?, // parametros
-        dos: Int? // parametros
-    ) : this(  // llamada constructor primario
-        if (uno == null) 0 else uno,
-        if (dos == null) 0 else uno
-    )
-
-    public fun sumar(): Int { // public por defecto, o usar private o protected
-        val total = numeroUno + numeroDos
-        agregarHistorial(total)
-        return total
-    }
-    companion object { // Atributos y Metodos "Compartidos"
-        // entre las instancias
-        val pi = 3.14
-        fun elevarAlCuadrado(num: Int): Int {
-            return num * num
-        }
-        val historialSumas = arrayListOf<Int>()
-        fun agregarHistorial(valorNuevaSuma:Int){
-            historialSumas.add(valorNuevaSuma)
+    /**
+     * Use BufferedWriter when number of write operations are more
+     * It uses internal buffer to reduce real IO operations and saves time
+     * @param data
+     * @param noOfLines
+     */
+    private fun writeUsingBufferedWriter(data: String, noOfLines: Int) {
+        val folder = File("Results")
+        val file = File(folder, "sample.txt")
+        var fr: FileWriter? = null
+        var br: BufferedWriter? = null
+        val dataWithNewLine = data + System.getProperty("line.separator")
+        try {
+            folder.mkdirs() // Create the folder if it doesn't exist
+            fr = FileWriter(file)
+            br = BufferedWriter(fr)
+            for (i in noOfLines downTo 1) {
+                br.write(dataWithNewLine)
+            }
+        } catch (e: IOException) {
+            e.printStackTrace()
+        } finally {
+            try {
+                br?.close()
+                fr?.close()
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
         }
     }
 }
-
-
-
-
-
-
-
--- Proyecto/Deber01/IntentoCompose/src/jvmMain/kotlin/Main.kt
+++ Proyecto/Deber01/IntentoCompose/src/jvmMain/kotlin/Main.kt
@@ -1,26 +1,283 @@
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.material.TextField
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 import androidx.compose.ui.window.rememberWindowState
+import java.text.SimpleDateFormat
+import java.util.*
+import java.io.*
+import androidx.compose.ui.graphics.Color
+
+val column1Weight = 0.2f
+val column2Weight = 0.2f
+val column3Weight = 0.2f
+val column4Weight = 0.2f
+val column5Weight = 0.2f
+
+data class Pharmacy(
+    var dateOfOpening: Date,
+    var isOpen: Boolean,
+    var pharmacyName: String,
+    var pharmacyID: Int,
+    var stockValue: Double,
+    var isModified: Boolean = false
+)
 
 
 fun main() = application {
+    val dateValue = remember { mutableStateOf("") }
+    val isOpenValue = remember { mutableStateOf("") }
+    val pharmacyNameValue = remember { mutableStateOf("") }
+    val pharmacyIDValue = remember { mutableStateOf("") }
+    val stockValue = remember { mutableStateOf("") }
+    val recordOfPharmacies = remember { mutableStateListOf<Pharmacy>() }
+    val isPrintClicked = remember { mutableStateOf(false) }
+    val selectedPharmacy = remember { mutableStateOf<Pharmacy?>(null) }
+
+    val file = File("RecordPharmacies.txt")
+    if (file.exists()) {
+        // If the file exists, read the pharmacies from it
+        readPharmaciesFromFile(file)?.let { recordOfPharmacies.addAll(it) }
+    }
+
     Window(
         onCloseRequest = ::exitApplication,
         title = "Farmacia",
-        state = rememberWindowState(width = 600.dp, height = 600.dp)
+        state = rememberWindowState(width = 1000.dp, height = 1000.dp)
     ) {
-        val count = remember { mutableStateOf(0) }
- 
+
+        MaterialTheme {
+            Column(Modifier.fillMaxSize(), Arrangement.spacedBy(5.dp)) {
+
+                TextField(
+                    value = pharmacyNameValue.value,
+                    onValueChange = { pharmacyNameValue.value = it },
+                    label = { Text("Pharmacy Name") }
+                )
+
+                TextField(
+                    value = pharmacyIDValue.value,
+                    onValueChange = { pharmacyIDValue.value = it },
+                    label = { Text("Pharmacy ID") }
+                )
+                TextField(
+                    value = dateValue.value,
+                    onValueChange = { dateValue.value = it },
+                    label = { Text("Date of Opening") }
+                )
+
+                TextField(
+                    value = isOpenValue.value,
+                    onValueChange = { isOpenValue.value = it },
+                    label = { Text("Is Open") }
+                )
+                TextField(
+                    value = stockValue.value,
+                    onValueChange = { stockValue.value = it },
+                    label = { Text("Stock Value") }
+                )
+
+                if (selectedPharmacy.value != null) {
+                    Button(
+                        onClick = {
+                            selectedPharmacy.value?.let { pharmacy ->
+                                pharmacy.pharmacyName = pharmacyNameValue.value
+                                pharmacy.pharmacyID = pharmacyIDValue.value.toIntOrNull() ?: 0
+                                val dateFormat = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
+                                pharmacy.dateOfOpening = dateFormat.parse(dateValue.value) ?: Date()
+                                pharmacy.isOpen = isOpenValue.value.toBoolean()
+                                pharmacy.stockValue = stockValue.value.toDoubleOrNull() ?: 0.0
+                                pharmacy.isModified = true
+
+                                // Update the recordOfPharmacies list with the modified pharmacy
+                                val index = recordOfPharmacies.indexOf(pharmacy)
+                                if (index != -1) {
+                                    recordOfPharmacies[index] = pharmacy
+                                }
+                                writePharmaciesToFile(file, recordOfPharmacies)
+                            }
+                            selectedPharmacy.value = null
+                        },
+                        modifier = Modifier.align(Alignment.CenterHorizontally)
+                    ) {
+                        Text("Save Changes")
+                    }
+                }
+                else {
+                    Button(
+                        onClick = {
+                            val dateFormat = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
+                            val parsedDate = dateFormat.parse(dateValue.value)
+                            val pharmacy = Pharmacy(
+                                dateOfOpening = parsedDate ?: Date(),
+                                isOpen = isOpenValue.value.toBoolean(),
+                                pharmacyName = pharmacyNameValue.value,
+                                pharmacyID = pharmacyIDValue.value.toIntOrNull() ?: 0,
+                                stockValue = stockValue.value.toDoubleOrNull() ?: 0.0
+                            )
+                            recordOfPharmacies.add(pharmacy)
+                            writePharmaciesToFile(file, recordOfPharmacies)
+                            println("Pharmacy: $pharmacy")
+                        },
+                        modifier = Modifier.align(Alignment.CenterHorizontally)
+                    ) {
+                        Text("Save Pharmacy")
+                    }
+                }
+
+                Button(
+                    onClick = { isPrintClicked.value = true },
+                    modifier = Modifier.align(Alignment.CenterHorizontally)
+                ) {
+                    Text("Print")
+                }
+
+                if (isPrintClicked.value) {
+                    TableScreen(recordOfPharmacies, onEditClicked = { pharmacy ->
+                        // Set the values of text fields with the data from the selected row
+                        pharmacyNameValue.value = pharmacy.pharmacyName
+                        pharmacyIDValue.value = pharmacy.pharmacyID.toString()
+                        dateValue.value = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault()).format(pharmacy.dateOfOpening)
+                        isOpenValue.value = pharmacy.isOpen.toString()
+                        stockValue.value = pharmacy.stockValue.toString()
+                        selectedPharmacy.value = pharmacy
+                    })
+                }
+            }
+        }
+    }
+}
+
+fun readPharmaciesFromFile(file: File): List<Pharmacy>? {
+    try {
+        val pharmacies = mutableListOf<Pharmacy>()
+        BufferedReader(FileReader(file)).use { reader ->
+            var line: String?
+            while (reader.readLine().also { line = it } != null) {
+                val parts = line!!.split(",")
+                val date = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault()).parse(parts[0])
+                val isOpen = parts[1].toBoolean()
+                val pharmacyName = parts[2]
+                val pharmacyID = parts[3].toInt()
+                val stockValue = parts[4].toDouble()
+                pharmacies.add(Pharmacy(date, isOpen, pharmacyName, pharmacyID, stockValue))
+            }
+        }
+        return pharmacies
+    } catch (e: IOException) {
+        e.printStackTrace()
+    }
+    return null
+}
+
+fun writePharmaciesToFile(file: File, pharmacies: List<Pharmacy>) {
+    try {
+        val writer = FileWriter(file)
+        pharmacies.forEach { pharmacy ->
+            val dateString = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault()).format(pharmacy.dateOfOpening)
+            val line = "$dateString,${pharmacy.isOpen},${pharmacy.pharmacyName},${pharmacy.pharmacyID},${pharmacy.stockValue}"
+            writer.write(line)
+            writer.write(System.lineSeparator())
+        }
+        writer.close()
+    } catch (e: IOException) {
+        e.printStackTrace()
+    }
+}
+
+
+@Composable
+fun TableScreen(recordOfPharmacies: List<Pharmacy>, onEditClicked: (Pharmacy) -> Unit) {
+    val tableData = recordOfPharmacies.map { pharmacy ->
+        val dateFormat = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
+        val formattedDate = dateFormat.format(pharmacy.dateOfOpening)
+        " ${pharmacy.pharmacyID}, ${pharmacy.pharmacyName}, $formattedDate, ${pharmacy.isOpen}, ${pharmacy.stockValue}"
+    }
+
+    LazyColumn(Modifier.fillMaxSize().padding(16.dp)) {
+        item {
+            Row() {
+                TableCell(text = "Pharmacy ID", weight = column1Weight)
+                TableCell(text = "Pharmacy Name", weight = column2Weight)
+                TableCell(text = "Date of Opening", weight = column3Weight)
+                TableCell(text = "Is Open", weight = column4Weight)
+                TableCell(text = "Stock Value", weight = column5Weight)
+                TableCell(text = "", weight = 0.2f) // Empty cell for the "Edit" button
+            }
+        }
+
+        items(recordOfPharmacies) { pharmacy ->
+            PharmacyRow(pharmacy = pharmacy, onEditClicked = onEditClicked)
+        }
+    }
+}
+
+@Composable
+fun RowScope.TableCell(
+    text: String,
+    weight: Float
+) {
+    Box(
+        Modifier
+            .weight(weight)
+            .padding(2.dp)
+            .background(Color.White)
+            .border(1.dp, Color.Black)
+    ) {
+        Text(
+            text = text,
+            Modifier.padding(2.dp)
+        )
+    }
+}
+
+@Composable
+fun PharmacyRow(
+    pharmacy: Pharmacy,
+    onEditClicked: (Pharmacy) -> Unit
+) {
+    val isSelected = remember { mutableStateOf(false) }
+
+    Row(Modifier.fillMaxWidth()) {
+        Text(
+            text = pharmacy.pharmacyID.toString(),
+            modifier = Modifier.weight(column1Weight)
+        )
+        Text(
+            text = pharmacy.pharmacyName,
+            modifier = Modifier.weight(column2Weight)
+        )
+        Text(
+            text = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault()).format(pharmacy.dateOfOpening),
+            modifier = Modifier.weight(column3Weight)
+        )
+        Text(
+            text = pharmacy.isOpen.toString(),
+            modifier = Modifier.weight(column4Weight)
+        )
+        Text(
+            text = pharmacy.stockValue.toString(),
+            modifier = Modifier.weight(column5Weight)
+        )
+        Button(
+            onClick = {
+                isSelected.value = !isSelected.value
+                onEditClicked(pharmacy)
+            },
+            modifier = Modifier.weight(column5Weight)
+        ) {
+            Text(if (isSelected.value) "Edit" else "Edit")
+        }
     }
 }
