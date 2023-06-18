56ae59143f2c3953351325138264a4ce57e64262
Ivanna-Daniela <ivanna.cevallos@epn.edu.ec>
2023-06-18 13:34:48 -0500

    Add tabs
    Three tabs: Welcome, add Pharmacy, add medicine

--- Proyecto/Deber01/IntentoCompose/src/jvmMain/kotlin/Main.kt
+++ Proyecto/Deber01/IntentoCompose/src/jvmMain/kotlin/Main.kt
@@ -1,12 +1,13 @@
+import Tab
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
-import androidx.compose.material.TextField
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.Edit
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -18,13 +19,17 @@ import java.text.SimpleDateFormat
 import java.util.*
 import java.io.*
 import androidx.compose.ui.graphics.Color
-
+enum class Tab {
+    Welcome,
+    Pharmacy,
+    Medicine
+}
 val column1Weight = 0.2f
 val column2Weight = 0.2f
 val column3Weight = 0.2f
 val column4Weight = 0.2f
-val column5Weight = 0.2f
-val column6Weight = 0.2f
+val column5Weight = 0.1f
+val column6Weight = 0.1f
 
 data class Pharmacy(
     var dateOfOpening: Date,
@@ -45,6 +50,7 @@ fun main() = application {
     val recordOfPharmacies = remember { mutableStateListOf<Pharmacy>() }
     val isPrintClicked = remember { mutableStateOf(false) }
     val selectedPharmacy = remember { mutableStateOf<Pharmacy?>(null) }
+    val currentTab = remember { mutableStateOf(Tab.Welcome) }
 
     val file = File("RecordPharmacies.txt")
     if (file.exists()) {
@@ -57,111 +63,150 @@ fun main() = application {
         title = "Farmacia",
         state = rememberWindowState(width = 1000.dp, height = 1000.dp)
     ) {
-
         MaterialTheme {
             Column(Modifier.fillMaxSize(), Arrangement.spacedBy(5.dp)) {
+                TabRow(selectedTabIndex = currentTab.value.ordinal) {
+                    Tab(
+                        selected = currentTab.value == Tab.Welcome,
+                        onClick = { currentTab.value = Tab.Welcome },
+                        text = { Text("Welcome") }
+                    )
+                    Tab(
+                        selected = currentTab.value == Tab.Pharmacy,
+                        onClick = { currentTab.value = Tab.Pharmacy },
+                        text = { Text("Pharmacy") }
+                    )
+                    Tab(
+                        selected = currentTab.value == Tab.Medicine,
+                        onClick = { currentTab.value = Tab.Medicine },
+                        text = { Text("Medicine") }
+                    )
+                }
 
-                TextField(
-                    value = pharmacyNameValue.value,
-                    onValueChange = { pharmacyNameValue.value = it },
-                    label = { Text("Pharmacy Name") }
-                )
-
-                TextField(
-                    value = pharmacyIDValue.value,
-                    onValueChange = { pharmacyIDValue.value = it },
-                    label = { Text("Pharmacy ID") }
-                )
-                TextField(
-                    value = dateValue.value,
-                    onValueChange = { dateValue.value = it },
-                    label = { Text("Date of Opening") }
-                )
-
-                TextField(
-                    value = isOpenValue.value,
-                    onValueChange = { isOpenValue.value = it },
-                    label = { Text("Is Open") }
-                )
-                TextField(
-                    value = stockValue.value,
-                    onValueChange = { stockValue.value = it },
-                    label = { Text("Stock Value") }
-                )
-
-                if (selectedPharmacy.value != null) {
-                    Button(
-                        onClick = {
-                            selectedPharmacy.value?.let { pharmacy ->
-                                pharmacy.pharmacyName = pharmacyNameValue.value
-                                pharmacy.pharmacyID = pharmacyIDValue.value.toIntOrNull() ?: 0
-                                val dateFormat = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
-                                pharmacy.dateOfOpening = dateFormat.parse(dateValue.value) ?: Date()
-                                pharmacy.isOpen = isOpenValue.value.toBoolean()
-                                pharmacy.stockValue = stockValue.value.toDoubleOrNull() ?: 0.0
-                                pharmacy.isModified = true
-
-                                // Update the recordOfPharmacies list with the modified pharmacy
-                                val index = recordOfPharmacies.indexOf(pharmacy)
-                                if (index != -1) {
-                                    recordOfPharmacies[index] = pharmacy
-                                }
-                                writePharmaciesToFile(file, recordOfPharmacies)
-                            }
-                            selectedPharmacy.value = null
-                        },
-                        modifier = Modifier.align(Alignment.CenterHorizontally)
-                    ) {
-                        Text("Save Changes")
+                when (currentTab.value) {
+                    Tab.Welcome -> {
+                        Box(
+                            Modifier.fillMaxSize().background(Color.Transparent),
+                            contentAlignment = Alignment.Center
+                        ) {
+                            Text("Bienvenido")
+                        }
                     }
-                } else {
-                    Button(
-                        onClick = {
-                            val dateFormat = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
-                            val parsedDate = dateFormat.parse(dateValue.value)
-                            val pharmacy = Pharmacy(
-                                dateOfOpening = parsedDate ?: Date(),
-                                isOpen = isOpenValue.value.toBoolean(),
-                                pharmacyName = pharmacyNameValue.value,
-                                pharmacyID = pharmacyIDValue.value.toIntOrNull() ?: 0,
-                                stockValue = stockValue.value.toDoubleOrNull() ?: 0.0
+                    Tab.Pharmacy -> {
+                        Column(Modifier.fillMaxSize(), Arrangement.spacedBy(5.dp)) {
+                            TextField(
+                                value = pharmacyNameValue.value,
+                                onValueChange = { pharmacyNameValue.value = it },
+                                label = { Text("Pharmacy Name") }
                             )
-                            recordOfPharmacies.add(pharmacy)
-                            writePharmaciesToFile(file, recordOfPharmacies)
-                            println("Pharmacy: $pharmacy")
-                        },
-                        modifier = Modifier.align(Alignment.CenterHorizontally)
-                    ) {
-                        Text("Save Pharmacy")
+
+                            TextField(
+                                value = pharmacyIDValue.value,
+                                onValueChange = { pharmacyIDValue.value = it },
+                                label = { Text("Pharmacy ID") }
+                            )
+                            TextField(
+                                value = dateValue.value,
+                                onValueChange = { dateValue.value = it },
+                                label = { Text("Date of Opening") }
+                            )
+
+                            TextField(
+                                value = isOpenValue.value,
+                                onValueChange = { isOpenValue.value = it },
+                                label = { Text("Is Open") }
+                            )
+                            TextField(
+                                value = stockValue.value,
+                                onValueChange = { stockValue.value = it },
+                                label = { Text("Stock Value") }
+                            )
+
+                            if (selectedPharmacy.value != null) {
+                                Button(
+                                    onClick = {
+                                        selectedPharmacy.value?.let { pharmacy ->
+                                            pharmacy.pharmacyName = pharmacyNameValue.value
+                                            pharmacy.pharmacyID = pharmacyIDValue.value.toIntOrNull() ?: 0
+                                            val dateFormat = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
+                                            pharmacy.dateOfOpening = dateFormat.parse(dateValue.value) ?: Date()
+                                            pharmacy.isOpen = isOpenValue.value.toBoolean()
+                                            pharmacy.stockValue = stockValue.value.toDoubleOrNull() ?: 0.0
+                                            pharmacy.isModified = true
+
+                                            // Update the recordOfPharmacies list with the modified pharmacy
+                                            val index = recordOfPharmacies.indexOf(pharmacy)
+                                            if (index != -1) {
+                                                recordOfPharmacies[index] = pharmacy
+                                            }
+                                            writePharmaciesToFile(file, recordOfPharmacies)
+                                        }
+                                        selectedPharmacy.value = null
+                                    },
+                                    modifier = Modifier.align(Alignment.CenterHorizontally)
+                                ) {
+                                    Text("Save Changes")
+                                }
+                            } else {
+                                Button(
+                                    onClick = {
+                                        val dateFormat = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
+                                        val parsedDate = dateFormat.parse(dateValue.value)
+                                        val pharmacy = Pharmacy(
+                                            dateOfOpening = parsedDate ?: Date(),
+                                            isOpen = isOpenValue.value.toBoolean(),
+                                            pharmacyName = pharmacyNameValue.value,
+                                            pharmacyID = pharmacyIDValue.value.toIntOrNull() ?: 0,
+                                            stockValue = stockValue.value.toDoubleOrNull() ?: 0.0
+                                        )
+                                        recordOfPharmacies.add(pharmacy)
+                                        writePharmaciesToFile(file, recordOfPharmacies)
+                                        println("Pharmacy: $pharmacy")
+                                    },
+                                    modifier = Modifier.align(Alignment.CenterHorizontally)
+                                ) {
+                                    Text("Save Pharmacy")
+                                }
+                            }
+
+                            Button(
+                                onClick = { isPrintClicked.value = true },
+                                modifier = Modifier.align(Alignment.CenterHorizontally)
+                            ) {
+                                Text("Print")
+                            }
+
+                            if (isPrintClicked.value) {
+                                TableScreen(recordOfPharmacies, onEditClicked = { pharmacy ->
+                                    // Set the values of text fields with the data from the selected row
+                                    pharmacyNameValue.value = pharmacy.pharmacyName
+                                    pharmacyIDValue.value = pharmacy.pharmacyID.toString()
+                                    dateValue.value =
+                                        SimpleDateFormat("yyyy-MM-dd", Locale.getDefault()).format(pharmacy.dateOfOpening)
+                                    isOpenValue.value = pharmacy.isOpen.toString()
+                                    stockValue.value = pharmacy.stockValue.toString()
+                                    selectedPharmacy.value = pharmacy
+                                }, onDeleteClicked = { pharmacy ->
+                                    recordOfPharmacies.remove(pharmacy)
+                                    writePharmaciesToFile(file, recordOfPharmacies)
+                                })
+                            }
+                        }
                     }
-                }
-
-                Button(
-                    onClick = { isPrintClicked.value = true },
-                    modifier = Modifier.align(Alignment.CenterHorizontally)
-                ) {
-                    Text("Print")
-                }
-
-                if (isPrintClicked.value) {
-                    TableScreen(recordOfPharmacies, onEditClicked = { pharmacy ->
-                        // Set the values of text fields with the data from the selected row
-                        pharmacyNameValue.value = pharmacy.pharmacyName
-                        pharmacyIDValue.value = pharmacy.pharmacyID.toString()
-                        dateValue.value =
-                            SimpleDateFormat("yyyy-MM-dd", Locale.getDefault()).format(pharmacy.dateOfOpening)
-                        isOpenValue.value = pharmacy.isOpen.toString()
-                        stockValue.value = pharmacy.stockValue.toString()
-                        selectedPharmacy.value = pharmacy
-                    }, onEliminateClicked = { pharmacy ->
-                        recordOfPharmacies.remove(pharmacy)
-                        writePharmaciesToFile(file, recordOfPharmacies)
-                    })
+                    Tab.Medicine -> {
+                        Box(
+                            Modifier.fillMaxSize().background(Color.White),
+                            contentAlignment = Alignment.Center
+                        ) {
+                            Text("Medicine")
+                        }
+                    }
                 }
             }
         }
     }
 }
+
 
 fun readPharmaciesFromFile(file: File): List<Pharmacy>? {
     try {
@@ -189,10 +234,8 @@ fun writePharmaciesToFile(file: File, pharmacies: List<Pharmacy>) {
     try {
         val writer = FileWriter(file)
         pharmacies.forEach { pharmacy ->
-            val dateString =
-                SimpleDateFormat("yyyy-MM-dd", Locale.getDefault()).format(pharmacy.dateOfOpening)
-            val line =
-                "$dateString,${pharmacy.isOpen},${pharmacy.pharmacyName},${pharmacy.pharmacyID},${pharmacy.stockValue}"
+            val dateString = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault()).format(pharmacy.dateOfOpening)
+            val line = "$dateString,${pharmacy.isOpen},${pharmacy.pharmacyName},${pharmacy.pharmacyID},${pharmacy.stockValue}"
             writer.write(line)
             writer.write(System.lineSeparator())
         }
@@ -204,11 +247,7 @@ fun writePharmaciesToFile(file: File, pharmacies: List<Pharmacy>) {
 
 
 @Composable
-fun TableScreen(
-    recordOfPharmacies: List<Pharmacy>,
-    onEditClicked: (Pharmacy) -> Unit,
-    onEliminateClicked: (Pharmacy) -> Unit
-) {
+fun TableScreen(recordOfPharmacies: List<Pharmacy>, onEditClicked: (Pharmacy) -> Unit, onDeleteClicked: (Pharmacy) -> Unit) {
     val tableData = recordOfPharmacies.map { pharmacy ->
         val dateFormat = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
         val formattedDate = dateFormat.format(pharmacy.dateOfOpening)
@@ -223,17 +262,13 @@ fun TableScreen(
                 TableCell(text = "Date of Opening", weight = column3Weight)
                 TableCell(text = "Is Open", weight = column4Weight)
                 TableCell(text = "Stock Value", weight = column5Weight)
-                TableCell(text = "", weight = 0.2f) // Empty cell for the "Edit" button
-                TableCell(text = "", weight = 0.2f) // Empty cell for the "Eliminate" button
+                TableCell(text = "Edit", weight = 0.1f) // Empty cell for the "Edit" button
+                TableCell(text = "Delete", weight = 0.1f) // Empty cell for the "Delete" button
             }
         }
 
         items(recordOfPharmacies) { pharmacy ->
-            PharmacyRow(
-                pharmacy = pharmacy,
-                onEditClicked = onEditClicked,
-                onEliminateClicked = onEliminateClicked
-            )
+            PharmacyRow(pharmacy = pharmacy, onEditClicked = onEditClicked, onDeleteClicked = onDeleteClicked)
         }
     }
 }
@@ -261,7 +296,7 @@ fun RowScope.TableCell(
 fun PharmacyRow(
     pharmacy: Pharmacy,
     onEditClicked: (Pharmacy) -> Unit,
-    onEliminateClicked: (Pharmacy) -> Unit
+    onDeleteClicked: (Pharmacy) -> Unit
 ) {
     val isSelected = remember { mutableStateOf(false) }
 
@@ -286,22 +321,17 @@ fun PharmacyRow(
             text = pharmacy.stockValue.toString(),
             modifier = Modifier.weight(column5Weight)
         )
-        Button(
-            onClick = {
-                isSelected.value = !isSelected.value
-                onEditClicked(pharmacy)
-            },
-            modifier = Modifier.weight(column5Weight)
+        IconButton(
+            onClick = { onEditClicked(pharmacy) },
+            modifier = Modifier.weight(column6Weight)
         ) {
-            Text(if (isSelected.value) "Edit" else "Edit")
+            Icon(Icons.Filled.Edit, contentDescription = "Edit")
         }
-        Button(
-            onClick = {
-                onEliminateClicked(pharmacy)
-            },
-            modifier = Modifier.weight(column5Weight)
+        IconButton(
+            onClick = { onDeleteClicked(pharmacy) },
+            modifier = Modifier.weight(column6Weight)
         ) {
-            Text("Eliminate")
+            Icon(Icons.Filled.Delete, contentDescription = "Delete")
         }
     }
 }
