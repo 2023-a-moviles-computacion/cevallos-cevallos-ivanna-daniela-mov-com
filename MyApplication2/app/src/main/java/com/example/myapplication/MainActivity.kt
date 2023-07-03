adbce933e9a4b6988e61a6436ed128d89976c9a7
Ivanna-Daniela <ivanna.cevallos@epn.edu.ec>
2023-07-03 11:57:40 -0500

    SQLITE

--- MyApplication2/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ MyApplication2/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,11 +1,47 @@
 package com.example.myapplication
 
+import android.app.Activity
+import android.app.Instrumentation.ActivityResult
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.ContactsContract
 import android.widget.Button
+import androidx.activity.result.contract.ActivityResultContracts
 
 class MainActivity : AppCompatActivity() {
+    val callbackContenidoIntentExplicito =
+        registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult()
+        ){
+            result->
+            if(result.resultCode == Activity.RESULT_OK){
+                if(result.data != null){
+                    val data = result.data
+                    "${data?.getStringExtra("nombreModificado")}"
+                }
+            }
+        }
+    val callbackIntentPickUri=
+        registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult()
+        ){
+            result->
+            if(result.resultCode === RESULT_OK){
+                if(result.data != null){
+                    val uri : Uri = result.data!!.data!!
+                    val cursor = contentResolver.query(uri, null, null, null, null, null)
+                    cursor?.moveToFirst()
+                    val indiceTelefono = cursor?.getColumnIndex(
+                        ContactsContract.CommonDataKinds.Phone.NUMBER
+                    )
+                    val telefono = cursor?.getString(indiceTelefono!!)
+                    cursor?.close()
+                    "Telefono${telefono}"
+                }
+            }
+        }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -20,6 +56,29 @@ class MainActivity : AppCompatActivity() {
             .setOnClickListener {
                 irActividad(BListView::class.java)
             }
+        val botonIntentImplicito = findViewById<Button>(R.id.btn_ir_intent_implicito)
+        botonIntentImplicito
+            .setOnClickListener{
+                val intentConRespuesta = Intent(
+                    Intent.ACTION_PICK,
+                    ContactsContract.CommonDataKinds.Phone.CONTENT_URI
+                )
+                callbackIntentPickUri.launch(intentConRespuesta)
+            }
+        val botonIntentExplicito = findViewById<Button>(R.id.btn_ir_intent_explicito)
+        botonIntentExplicito
+            .setOnClickListener{
+                abrirArctividadConParametros(CIntentExplicitoParametros::class.java)
+            }
+    }
+    fun abrirArctividadConParametros(
+        clase : Class<*>
+    ){
+        val intentExplicito = Intent(this, clase)
+        intentExplicito.putExtra("nombre","Adrian")
+        intentExplicito.putExtra("apellido", "Eguez")
+        intentExplicito.putExtra("adad", 34)
+        callbackContenidoIntentExplicito.launch(intentExplicito)
     }
     fun irActividad(
         clase: Class<*>
